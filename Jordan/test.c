b7eb0d4a81ccf42a9f3ae3b2f641580352b53869
Radosław Głombiowski <predoobcy@gmail.com>
2012-12-17 14:33:57 +0100

    Final

--- Jordan/test.c
+++ Jordan/test.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(){
-  int n, i;
+  int n, i, parent, left, right;
   FILE *fp;
   printf("Podaj liczbe elementow: ");
   scanf("%i", &n);
@@ -11,12 +11,29 @@ int main(){
     printf("BLAD\n");
     exit(1);
   }
+
+  fprintf(fp,"%i\n", n);
+
   for(i=1; i<=n; i++){
+    parent = i/2-1;
+    left = 2*i-1;
+    right = 2*i;
     if(i==1){
-      fprintf(fp, "%i %i\n", (2*i-1), (2*i));
+      fprintf(fp, "%i %i -1\n", left, right);
       continue;
     }
-    fprintf(fp,"%i %i %i\n", (i/2-1), (2*i-1), (2*i));
+    if(right < n && left < n){
+      fprintf(fp,"%i %i %i -1\n", parent, left, right);
+      continue;
+    }
+    if(right >= n && left < n){
+      fprintf(fp, "%i %i -1\n", parent, left);
+      continue;
+    }
+    if(left > n){
+      fprintf(fp, "%i -1\n", parent);
+      continue;
+    }
   }
   fclose (fp);
 
